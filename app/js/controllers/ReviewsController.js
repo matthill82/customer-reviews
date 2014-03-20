d5c2b2d6c87384bcdd913f93f473abcf32b5c6d7
matthew hill <matthill82@live.com>
2014-03-20 21:20:55 +0000

    fucking hell, i hate angular

--- app/js/controllers/ReviewsController.js
+++ app/js/controllers/ReviewsController.js
@@ -2,9 +2,53 @@
 
 reviewsApp.controller('ReviewsController',
 	function RatingsController ($scope, reviewData) {
-		$scope.sortorder = 'rating';
+
 		reviewData.getReviews(function (event) {
 			$scope.event = event;
 		});
+//        $scope.sortorder = 'UserOverallRating';
+
+        //Contains the sorting options
+        $scope.sortOptions = {
+            stores : [
+                { id : 1, sortBy : 'Most Recent', UserOverallRating : 1 },
+                { id : 2, sortBy : 'Oldest', UserOverallRating:2 },
+                { id : 3, sortBy : 'Lowest Score', UserOverallRating:3 },
+                { id : 4, sortBy : 'Highest', UserOverallRating:4 }
+            ]
+        }
+
+        //Contains the filter options
+        $scope.filterOptions = {
+            stores: [
+                {id : 5, name : 'Show All', rating: 5 },
+                {id : 1, name : 'Most Recent', rating: 1 },
+                {id : 2, name : 'Oldest', rating: 2 },
+                {id : 3, name : 'Lowest Rating', rating: 3 },
+                {id : 4, name : 'Highest Rating', rating: 4 },
+
+            ]
+        };
+
+        //Mapped to the model to filter
+        $scope.filterItem = {
+            store: $scope.filterOptions.stores[0]
+        }
+
+        //Mapped to the model to sort
+        $scope.sortItem = {
+            store: $scope.sortOptions.stores[0]
+        };
+
+        //Custom filter - filter based on the UserOverallRating selected
+        $scope.customFilter = function (data) {
+            if (data.UserOverallRating === $scope.filterItem.store.UserOverallRating) {
+                return true;
+            } else if ($scope.filterItem.store.rating === 5) {
+                return true;
+            } else {
+                return false;
+            }
+        };
 	}
 );
